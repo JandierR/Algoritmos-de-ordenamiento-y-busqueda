6de9dc29e28e9ba883056df95442ecedadb3b9de
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-04-07 15:08:33 -0600

    Metodo llenarArreglo agregado

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    private static String[] palabras;
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        llenarArreglo();
+
+        System.out.println(Arrays.toString(palabras));
+    }
+
+    public static void llenarArreglo() throws IOException {
+        System.out.println();
+        System.out.print("Ingrese el tamaño del arreglo: ");
+        int tamano = Integer.parseInt(in.readLine());
+
+        palabras = new String[tamano];
+
+        System.out.println("A continuación, llene el arreglo con palabras: ");
+
+        for (int i = 0; i < palabras.length; i++) {
+            System.out.print("Palabra a ingresar -> ");
+
+            palabras[i] = in.readLine();
+        }
+        System.out.println("Enhorabuena. Ha llenado el arreglo de palabras!!!");
+    }
+
+
+
+}
